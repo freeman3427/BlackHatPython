583d83e7192ae66555a3af8e8a49e57e3150dd34
lyc3427@gmail.com <freeman3427@gmail.com>
2024-11-14 23:07:58 +0900

    asfg

--- netcat.py
+++ netcat.py
@@ -16,6 +16,45 @@ def execute(cmd):
     return output.decode()
 
 
+class NetCat:
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+
+        try:
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                if response:
+                    print(response)
+                    buffer = input('> ')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='BHP Net Tool',
@@ -36,50 +75,11 @@ if __name__ == '__main__':
                         default=5555, help='specified port')
     parser.add_argument(
         '-t', '--target', default='192.168.1.203', help='specified IP')
-    parser.add_argument('-u','--upload', help='upload file')
-    args=parser.parse_args()
+    parser.add_argument('-u', '--upload', help='upload file')
+    args = parser.parse_args()
     if args.listen:
-        buffer=''
+        buffer = ''
     else:
-        buffer=sys.stdin.read()
-        nc=NetCat(args,buffer.encode('utf-8'))
-        nc.run()
-
-
-    class NetCat:
-        def __init__(self,args,buffer=None):
-            self.args=args
-            self.buffer=buffer
-            self.socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-        def run(self):
-            if self.args.listen:
-                self.listen()
-            else:
-                self.send()
-
-        def send(self):
-            self.socket.connect((self.args.target, self.args.port))
-            if self.buffer:
-                self.socket.send(self.buffer)
-
-            try:
-                while True:
-                    recv_len=1
-                    response=''
-                    while recv_len:
-                        data=self.socket.recv(4096)
-                        recv_len=len(data)
-                        response+=data.decode()
-                        if recv_len < 4096:
-                            break
-                    if response:
-                        print(response)
-                        buffer=input('> ')
-                        buffer+='\n'
-                        self.socket.send(buffer.encode())
-            except KeyboardInterrupt:
-                print('User terminated.')
-                self.socket.close()
-                sys.exit()
-
+        buffer = sys.stdin.read()
+    nc = NetCat(args, buffer.encode('utf-8'))
+    nc.run()
